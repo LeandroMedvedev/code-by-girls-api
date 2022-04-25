9c4eaabbd52e4a6b94289bb8b15e6053704ae537
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-04-25 13:53:55 -0300

    fix: modified table name from users_groups to users_groups_table

--- app/models/user_group_table.py
+++ app/models/user_group_table.py
@@ -1,11 +1,8 @@
-from sqlalchemy.sql.schema import Column, ForeignKey
-from sqlalchemy.sql.sqltypes import Integer
-
 from app.configs import db
 
-users_groups = db.Table(
+users_groups_table = db.Table(
     "users_groups",
-    Column("id", Integer, primary_key=True),
-    Column("user_id", Integer, ForeignKey("users.id")),
-    Column("group_id", Integer, ForeignKey("groups.id")),
+    db.Column("id", db.Integer, primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("group_id", db.Integer, db.ForeignKey("groups.id")),
 )
