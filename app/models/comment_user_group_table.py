50de27b94fa0c5f2d68fa164530d78faf7b8a87f
Matheus Serafim <matheus18serafim@gmail.com>
2022-04-25 12:09:11 -0300

    UPDATE: Column timestamp

--- app/models/comment_user_group_table.py
+++ app/models/comment_user_group_table.py
@@ -6,5 +6,5 @@ comments_users_groups_table = db.Table("comments_users_groups",
     db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
     db.Column("group_id", db.Integer, db.ForeignKey("groups.id")),
     db.Column("comments", db.Text),
-    db.Column("timestemp", db.Date),
+    db.Column("timestamp", db.Date),
 )
