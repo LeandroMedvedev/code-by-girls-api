9ff8019304b27ad50c7c7e687ed0044019f4dae3
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-04-25 18:06:48 -0300

    feat:model=UserModel, Controller=User

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,12 +1,28 @@
 from dataclasses import dataclass
-
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import Integer
+from sqlalchemy.sql.sqltypes import Integer,String
 
 from app.configs import db
 
 
 @dataclass
-class User(db.Model):
+class UserModel(db.Model):
+
+    id:int
+    name:str
+    email:str
+    skills:list
+    works:list
+    # password:str
 
     __tablename__ = "users"
+
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(50), nullable = False)
+    email = Column(String(100), nullable = False, unique = True)
+    password = Column(String)
+
+    skills = relationship("SkillModel", backref="user")
+    works = relationship("WorkModel", backref="user")
