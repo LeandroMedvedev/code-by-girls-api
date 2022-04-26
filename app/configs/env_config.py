4e1d7ee119eb84be39bb6aa560903b0480a0199a
Matheus Serafim <matheus18serafim@gmail.com>
2022-04-26 10:24:27 -0300

    feat:creation login

--- app/configs/env_config.py
+++ app/configs/env_config.py
@@ -9,3 +9,4 @@ def init_app(app: Flask):
     app.config["JSON_SORT_KEYS"] = env("JSON_SORT_KEYS")
     app.config["SQLALCHEMY_DATABASE_URI"] = env("SQLA_DB_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = env("SQLA_TRACK_MOD")
+    app.config["JWT_SECRET_KEY"] = env("SECRET_KEY")
