eb20e352f4bfe9853b7264991bf29fa6c344a4ec
marta <martachmlima@gmail.com>
2022-04-26 20:06:34 -0300

    feature: adding login route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,11 +2,13 @@ from flask import Blueprint
 from flask import Flask
 
 from app.routes.user_blueprint import bp_user
+from app.routes.login_blueprint import bp_login
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
 
 
 def init_app(app: Flask):
     bp_api.register_blueprint(bp_user)
+    bp_api.register_blueprint(bp_login)
 
     app.register_blueprint(bp_api)
