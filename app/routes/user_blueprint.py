07dd3d9a55a69ff3c29152fcf2a4fd6156e0d5ab
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-04-27 10:08:32 -0300

    fix: slash in blueprint url_prefix

--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app.controllers.users_controller import create_user,get_user,att_user,delete_user
 
 
-bp_user = Blueprint("users",__name__, url_prefix ='users')
+bp_user = Blueprint("users", __name__, url_prefix ='/users')
 bp_user.post("")(create_user)
 bp_user.get("")(get_user)
 bp_user.patch("<int:id>")(att_user)
