bf4fcd8b260d524149e34063d451d7441cc3e13b
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-04-28 15:36:37 -0300

    fix: url_prefix included in group_blueprint

--- app/routes/group_blueprint.py
+++ app/routes/group_blueprint.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 from app.controllers import group_controller
 
-bp = Blueprint('groups', __name__)
+bp = Blueprint('groups', __name__, url_prefix='/groups')
 
-bp.post('/group')(group_controller.create_group)
-bp.get('/groups')(group_controller.get_groups)
-bp.get('group/<int:id>')(group_controller.get_group_by_id)
-bp.patch('group/<int:id>')(group_controller.update_group)
-bp.delete('group/<int:id>')(group_controller.delete_group)
+bp.post('')(group_controller.create_group)
+bp.get('')(group_controller.get_groups)
+bp.get('/<int:id>')(group_controller.get_group_by_id)
+bp.patch('/<int:id>')(group_controller.update_group)
+bp.delete('/<int:id>')(group_controller.delete_group)
