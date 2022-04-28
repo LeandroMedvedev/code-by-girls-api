ce634f46faad8e081e5e9f4f0862c2e689030ee0
marta <martachmlima@gmail.com>
2022-04-28 19:38:04 -0300

    feat: treatig possible errors when creating user

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,6 @@
 from .group_service import check_data
 from .group_service import get_by_id
+from .user_service import check_user_data
+from .user_service import check_mandatory_keys
+from .user_service import normalize_data
+from .user_service import check_value_type
